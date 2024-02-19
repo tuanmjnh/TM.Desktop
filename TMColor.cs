0d7fe9955e9d39cfe79192c40993030925495da6
tuanmjnh <minhtuan200990@gmail.com>
2024-02-19 10:23:57 -0500

    2024.02.19.1 - update add text

---
+++ TMColor.cs
@@ -0,0 +1,14 @@
+﻿namespace TM.Desktop
+{
+    public static class TMColor
+    {
+        public static string ColorToHexDecimal(this System.Drawing.Color color, string prefix = "#")
+        {
+            return $"{prefix}{color.R:X2}{color.G:X2}{color.B:X2}";
+        }
+        public static System.Drawing.Color HexDecimalToColor(this string hexDecimal)
+        {
+            return System.Drawing.ColorTranslator.FromHtml(hexDecimal);
+        }
+    }
+}
--- obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d61cd96c12d9b040edb0e011a4c29b1560e63d94")]
 [assembly: System.Reflection.AssemblyProductAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
