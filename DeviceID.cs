ec7488e74864d6868b0a01cf4f7d203df7eab5d3
tuanmjnh <minhtuan200990@gmail.com>
2024-04-25 21:15:50 +0700

    2024.04.25.1 - add view DeviceID

---
+++ DeviceID.cs
@@ -0,0 +1,28 @@
+﻿using DeviceId;
+using DeviceId.Windows;
+using DeviceId.Windows.Wmi;
+using DeviceId.Linux;
+//using DeviceId.Mac;
+namespace TM.Desktop
+{
+    public static class DeviceID
+    {
+        public static string get()
+        {
+            return new DeviceIdBuilder()
+                 .AddMachineName()
+                 .AddOsVersion()
+                 .OnWindows(windows => windows
+                     //.AddProcessorId()
+                     .AddMotherboardSerialNumber()
+                     .AddSystemDriveSerialNumber())
+                 .OnLinux(linux => linux
+                     .AddMotherboardSerialNumber()
+                     .AddSystemDriveSerialNumber())
+                 .OnMac(mac => mac
+                     .AddSystemDriveSerialNumber()
+                     .AddPlatformSerialNumber())
+                 .ToString();
+        }
+    }
+}
--- obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f8c5b5019fba8b43dc8303c2eb9840ec3b8b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69cac7f7bf7ad32ffb2365ea053c725830e5d05a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
