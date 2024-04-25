e51bdeade6db82e94feef49d43cfbd1cbd44c083
tuanmjnh <minhtuan200990@gmail.com>
2024-04-25 23:07:49 +0700

    2024.04.25.2

--- obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7488e74864d6868b0a01cf4f7d203df7eab5d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca00b1e2e5e250d39a31a320b2ba70360c2175d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
