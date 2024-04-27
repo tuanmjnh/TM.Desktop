08139f7618fe14249d9a97ab002476b00f6f9534
tuanmjnh <minhtuan200990@gmail.com>
2024-04-27 18:12:50 +0700

    2024.04.27.1 - update manager user

--- obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee771709e4e7fbf4edfa689faacf2ba7ec6a24e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e44c5a6ecf17f7486ffbafb8f95462f1a4d02b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
