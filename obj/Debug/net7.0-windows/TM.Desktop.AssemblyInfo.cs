964be4843a3da3ccd42eaa2aadf368a4f0ca131b
tuanmjnh <minhtuan200990@gmail.com>
2024-05-29 16:46:57 +0700

    2024.05.29.1 - update render

--- obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
+++ obj/Debug/net7.0-windows/TM.Desktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54fb4f04b260a21b50c9c79231d2a6fc5233aafd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ce85859b13e81dadcb3326939248e163653453")]
 [assembly: System.Reflection.AssemblyProductAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TM.Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
